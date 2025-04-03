9bc13a37976ffd592cbe77165ca8d9d5f7a2adcc
kiriyama <13514904+kiriyamagitee@user.noreply.gitee.com>
2025-04-04 00:13:02 +0800

    添加了动态页面内容获取

--- src/stores/contentsotre.ts
+++ src/stores/contentsotre.ts
@@ -1,4 +1,5 @@
-// src/stores/contentsotre.ts 此页用来动态管理显示的回复数据 清空请使用 sotre.contentdata = []
+// src/stores/contentsotre.ts 此页用来动态管理显示的回复数据 
+//清空请使用 sotre.carddata.splice(0,cardstore.carddata.length); //清空列表
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import type{ sendcarddata } from '../types/sendcard';
